b56348400edeab211ef16b492b3b4cec5b4e4ab6
Calvin De Lima <calvindelima@Calvins-MacBook-Pro.local>
2014-05-31 17:33:48 -0400

    Cleanup

--- src/main/scala/org/cdl/scalding/Features.scala
+++ src/main/scala/org/cdl/scalding/Features.scala
@@ -7,58 +7,53 @@ class Features(args : Args) extends Job(args) {
 
   def dirs = Map("d" -> "-debug.csv", "p" -> "-prod.csv")
 
-  // Inputs
+  // input args
   val trainHistoryFile = args("trainHistory")
   val testHistoryFile = args("testHistory")
   val transactionsFile = args("transactions")
   val offersFile = args("offers")
 
-  // Debug outputs
-
-  //id,chain,offer,market,repeattrips,repeater,offerdate
+  // csv files
   val train = Csv(trainHistoryFile, separator = ",", skipHeader = true)
   val test = Csv(testHistoryFile, separator = ",",  skipHeader=true)
   val transactions = Csv(transactionsFile, separator = ",",  skipHeader=true)
   val offers = Csv(offersFile, separator = ",", skipHeader = true)
 
+  // debug
   val dbg = PipeDebug().toStdOut.printFieldsEvery(Some(5))
 
+  // repeater counts for offers
   val repeaters =
     train
       .filter('repeater ) { repeater : String => repeater == "t" }
       .groupBy('offer) { _.size }
       .rename('size -> 'numRepeaters)
 
-  //  transactions with training data
+  //  transactions with training and offer data
   val trainOffersTransaction =
     transactions
       .discard('category, 'brand, 'company, 'date)
         .joinWithSmaller(('id,'chain) -> ('id,'chain), train) // train*
         .joinWithTiny('offer -> 'offer, offers) // offers
         .joinWithSmaller('offer -> 'offer, repeaters)
-//      .debug(dbg)
-//      .write(NullSource)
 
-
-//id,chain,dept,category,company,brand,date,productsize,productmeasure,purchasequantity,purchaseamount
-
-  // Features schemas
+  // feature schemas
   val productSizeFeatures = ('productSizeCount, 'productSizeMean, 'productSizeStdDev)
   val purchaseQuantityFeatures = ('purchaseQuantityCount, 'purchaseQuantityMean, 'purchaseQuantityStdDev)
   val purchaseAmountFeatures = ('purchaseAmountCount, 'purchaseAmountMean,'purchaseAmountStdDev)
   val repeaterFeatures = ('repeaterCount, 'repeaterMean, 'repeaterStdDev)
 
-  val offerFeatures =
+  // labelled data
+  val trainingData =
     trainOffersTransaction
       .groupBy('id, 'offer, 'category, 'brand, 'company, 'chain, 'dept, 'productmeasure) {
         _.sizeAveStdev('productsize -> productSizeFeatures)
-          .sizeAveStdev('purchasequantity -> purchaseQuantityFeatures)
-          .sizeAveStdev('purchaseamount -> purchaseAmountFeatures)
-//          .sizeAveStdev('numRepeaters -> repeaterFeatures)
+         .sizeAveStdev('purchasequantity -> purchaseQuantityFeatures)
+         .sizeAveStdev('purchaseamount -> purchaseAmountFeatures)
+       //.sizeAveStdev('numRepeaters -> repeaterFeatures)
       }
       .joinWithSmaller(('id, 'chain, 'offer) -> ('id, 'chain, 'offer), train)
       .discard('repeattrips, 'offerdate)
       .debug(dbg)
       .write(NullSource)
-
 }
